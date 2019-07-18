6403825db984aace92129ff3671be67eaf5f4c7b
Loraine GAULTIER <lgaultie@e1r5p1.42.fr>
2019-07-18 19:41:05 +0200

    parsing ongoing: rooms and links added, leaks done

--- src/free.c
+++ src/free.c
@@ -6,31 +6,44 @@
 /*   By: lgaultie <lgaultie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 16:35:46 by lgaultie          #+#    #+#             */
-/*   Updated: 2019/07/18 18:35:42 by lgaultie         ###   ########.fr       */
+/*   Updated: 2019/07/18 19:31:00 by lgaultie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lem_in.h>
 
+void		free_links(t_links *links)
+{
+	t_links		*tmp;
+
+	while (links->next)
+	{
+		tmp = links;
+		links = links->next;
+		free(tmp->name);
+		free(tmp);
+	}
+	if (!(links->next))
+	{
+		free(links->name);
+		free(links);
+	}
+}
+
+/*
+** free_list: free les links quand ils existent
+*/
+
 void		free_list(t_rooms *rooms)
 {
 	t_rooms		*tmp;
-	// t_links		*tmp2;
 
 	while (rooms)
 	{
+		if (rooms->links)
+			free_links(rooms->links);
 		tmp = rooms;
 		rooms = rooms->next;
-		// if (rooms->links)
-		// {
-		// 	while (rooms->links && rooms->links->next)
-		// 	{
-		// 		tmp2 = rooms->links;
-		// 		rooms->links = rooms->links->next;
-		// 		free(tmp2->name);
-		// 		free(tmp2);
-		// 	}
-		// }
 		free(tmp->name);
 		free(tmp);
 	}
