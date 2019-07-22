0767c016cb6ac6a6feb3745fc83d4e7d0682c31d
Loraine GAULTIER <lgaultie@e1r5p1.42.fr>
2019-07-22 13:19:03 +0200

    corrected small bug on check_start_end --> variable error was redefined when it shouldn't

--- src/read_input.c
+++ src/read_input.c
@@ -6,30 +6,36 @@
 /*   By: lgaultie <lgaultie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 18:47:08 by lgaultie          #+#    #+#             */
-/*   Updated: 2019/07/19 13:38:13 by lgaultie         ###   ########.fr       */
+/*   Updated: 2019/07/22 11:57:39 by lgaultie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lem_in.h>
 
-static int  check_input(t_farm *farm)
-{
-    int     error;
-    int     check_startend;
-    t_rooms *tmp_rooms;
+/*
+** check_input: reparcours la liste pour verifier si il y a une
+** salle start et end.
+** (Peut etre le faire directement dans read_input pour optimiser --> evite
+** de parcourir la liste, à voir plus tard si on a besoin de gagner du temps
+** d'execution, et si ça nous fera vraiment gagner du temps ou pas)
+*/
 
-    error = 0;
-    check_startend = 0;
-    tmp_rooms = farm->rooms;
-    while (tmp_rooms)
-    {
-        if (tmp_rooms->start_end == 1 || tmp_rooms->start_end == 2)
-            check_startend++;
-        tmp_rooms = tmp_rooms->next;
-    }
-    if (check_startend != 2)
-        error = -1;
-    return (error);
+static int	check_start_end(int error, t_farm *farm)
+{
+	int		check_startend;
+	t_rooms	*tmp_rooms;
+
+	check_startend = 0;
+	tmp_rooms = farm->rooms;
+	while (tmp_rooms)
+	{
+		if (tmp_rooms->start_end == 1 || tmp_rooms->start_end == 2)
+			check_startend++;
+		tmp_rooms = tmp_rooms->next;
+	}
+	if (check_startend != 2)
+		error = -1;
+	return (error);
 }
 
 /*
@@ -61,13 +67,14 @@ int		    read_input(t_farm *farm)
 		if (line)
 			free(line);
 		ret = get_next_line(0, &line);
-		ft_putendl(line);
+		if (ret > 0)
+			ft_putendl(line);
 		if (line && ft_strcmp(line, "##start") == 0)
 			start_end = 1;
 		else if (line && ft_strcmp(line, "##end") == 0)
 			start_end = 2;
 		if (line_nb == 1)
-		    error = check_ants(farm, line);
+			error = check_ants(farm, line);
 		if (line && error == 0 && line[0] != '#')
 		{
 			if (parse(farm, line, start_end) == ERROR)
@@ -79,20 +86,20 @@ int		    read_input(t_farm *farm)
 		line_nb++;
 		ft_memdel((void**)&line);
 	}
-    error = check_input(farm);
+	error = check_start_end(error, farm);
 
 	/*while (farm->rooms)
-    {
-        printf("x_pos = %d, y_pos = %d, name = %s, id = %d, start-end = %d\n", farm->rooms->x_pos, farm->rooms->y_pos, farm->rooms->name, farm->rooms->room_id, farm->rooms->start_end);
-        if (farm->rooms->links)
-        {
-            while (farm->rooms->links)
-            {
-                printf("links-name = %s\n", farm->rooms->links->name);
-                farm->rooms->links = farm->rooms->links->next;
-            }
-        }
-        farm->rooms = farm->rooms->next;
+	{
+		printf("x_pos = %d, y_pos = %d, name = %s, id = %d, start-end = %d\n", farm->rooms->x_pos, farm->rooms->y_pos, farm->rooms->name, farm->rooms->room_id, farm->rooms->start_end);
+		if (farm->rooms->links)
+		{
+			while (farm->rooms->links)
+			{
+				printf("links-name = %s\n", farm->rooms->links->name);
+				farm->rooms->links = farm->rooms->links->next;
+			}
+		}
+		farm->rooms = farm->rooms->next;
 	}*/
 	return (error);
 }
