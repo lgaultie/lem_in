d1a48a70bade23b02811d0e60ff54759d0bc4db3
Christelle <christelle.mouele@gmail.com>
2019-09-11 00:16:40 +0200

    fix special case in ants_per_paths(), begin implementation of send_ants()

--- src/algo/move_ants.c
+++ src/algo/move_ants.c
@@ -2,6 +2,48 @@
 
 #include <lem_in.h>
 
+// Créer un tableau contenant, pour chaque fourmi, un compteur: commence a 0,
+// si la fourmi bouge dune case le compteur sincremente de 1, etc...
+// si la fourmi a bouge de 10 case, le compteur est a 10, on regarde dans le tableau
+// de path a quel id de salle correspond lindex 10, et on a sa position: l'id de la
+// salle dans laquelle elle est, on met un int visited a 1 : la salle est occupee
+// si le compteur est egale a la taille du chemin, on ne lincremente plus, la fourmi est
+// arrivee
+
+/*
+** send_ants()
+*/
+
+void	send_ants(t_farm *farm, int index_of_set)
+{
+	/*int	*ants;
+	int	arrived;
+
+	arrived = 0;
+	if (!(ants = ft_memalloc(sizeof(int) * farm->ants)))
+	return ;
+	while (arrived < farm->ants)
+	{
+		//si ants[x] = salle end, alors arrived++
+	}*/
+
+	int 	id_ant;
+	t_paths	*tmp;
+
+	id_ant = 1;
+	tmp = farm->sets[index_of_set];
+	while (tmp)
+	{
+		printf("ants a envoyer = %d\n", tmp->ants_to_send);
+		// la fourmi visite sur une salle du chemin
+		// on met la salle d'où elle vient à visited = 0
+		// on met la salle où elle est à visited = 1
+		// si la fourmi doit aller sur une salle déjà visited, on ne fait rien
+		// om imprime
+		tmp = tmp->next;
+	}
+}
+
 /*
 ** choose_set() searches for the best set of paths to use (the one with the
 ** minimum moves number of moves). It returns the index of the optimized set.
@@ -77,42 +119,18 @@ void	ants_per_paths(t_farm *farm)
 	}
 	printf("total de fourmis dans la map = %d\n", farm->ants);
 	printf("total de fourmis envoyées dans les chemins = %d\n", total_ants_sent);
-	if (total_ants_sent < farm->ants)
+	tmp = farm->sets[index_of_set];
+	while (tmp)
 	{
-		printf("on renvoie des fourmis !\n");
-		tmp = farm->sets[index_of_set];
-		while (total_ants_sent < farm->ants)
+		if (total_ants_sent < farm->ants)
 		{
-			while (tmp)
-			{
-				tmp->ants_to_send++;
-				tmp = tmp->next;
-			}
+			printf("on renvoie une fourmi !\n");
+			tmp->ants_to_send++;
 			total_ants_sent++;
 		}
+		tmp = tmp->next;
 	}
 	printf("total de fourmis dans la map = %d\n", farm->ants);
 	printf("total de fourmis envoyées dans les chemins = %d\n", total_ants_sent);
-	// send_ants(farm);
+	send_ants(farm, index_of_set);
 }
-
-// // Créer un tableau contenant, pour chaque fourmi, un compteur: commence a 0,
-// // si la fourmi bouge dune case le compteur sincremente de 1, etc...
-// // si la fourmi a bouge de 10 case, le compteur est a 10, on regarde dans le tableau
-// // de path a quel id de salle correspond lindex 10, et on a sa position: l'id de la
-// // salle dans laquelle elle est, on met un int visited a 1 : la salle est occupee
-// // si le compteur est egale a la taille du chemin, on ne lincremente plus, la fourmi est
-// // arrivee
-// void		send_ants(t_farm *farm)
-// {
-// 	int		*ants;
-// 	int		arrived;
-
-// 	arrived = 0;
-// 	if (!(ants = ft_memalloc(sizeof(int) * farm->ants)))
-// 		return ;
-// 	while (arrived < farm->ants)
-// 	{
-// 		//si ants[x] = salle end, alors arrived++
-// 	}
-// }
